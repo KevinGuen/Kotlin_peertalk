15977d7c8fefef390bd5142e2e05ba8e1f0a68cf
Kevin <parkdk92@gmail.com>
2021-06-16 19:00:33 +0900

    0616

--- app/src/main/java/co/kr/itforone/study_01_fragment/Adapters/MycontAdapter.kt
+++ app/src/main/java/co/kr/itforone/study_01_fragment/Adapters/MycontAdapter.kt
@@ -1,45 +1,64 @@
 package co.kr.itforone.study_01_fragment.Adapters
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
+import co.kr.itforone.study_01_fragment.R
 import co.kr.itforone.study_01_fragment.dataClases.NonelistItem
+import co.kr.itforone.study_01_fragment.databinding.ListitemNoneBinding
 
-class MycontAdapter  : RecyclerView.Adapter<MycontAdapter.ViewHolder>() {
+class MycontAdapter(var datas: MutableList<NonelistItem>) :
+    RecyclerView.Adapter<MycontAdapter.ViewHolder>() {
 
-    var datas = mutableListOf<NonelistItem>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
-        val view = LayoutInflater.from(parent.context).inflate(, parent, false)
+        Log.d("datas_size2", datas.size.toString())
+        val binding =
+            ListitemNoneBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
 
-        return view
-
-        TODO("Not yet implemented")
     }
 
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Log.d("datas_size2", position.toString())
+        holder.onbind(datas.get(position))
 
-
-        TODO("Not yet implemented")
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        Log.d("datas_size2", datas.size.toString())
+        return datas.size
     }
 
 
-    inner class ViewHolder (view : View) : RecyclerView.ViewHolder(view) {
+    inner class ViewHolder(var binding: ListitemNoneBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        var tv_1: TextView = binding.noneitemTv
+        var tv_2: TextView = binding.noneitemImg
+        var tv_3: TextView = binding.noneitemCall
 
 
+        fun onbind(data: NonelistItem) {
+            if(data.name!=null) {
+                tv_1.text = data.name
+            }
+            if(data.src!=null) {
+                tv_2.text = data.src
+            }
+            if(data.number!=null) {
+                tv_3.text = data.number
+            }
 
 
-
+        }
 
     }
-
-
 }
--- app/src/main/java/co/kr/itforone/study_01_fragment/fragment/Mycont.kt
+++ app/src/main/java/co/kr/itforone/study_01_fragment/fragment/Mycont.kt
@@ -1,31 +1,42 @@
 package co.kr.itforone.study_01_fragment.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import co.kr.itforone.study_01_fragment.Adapters.MycontAdapter
 import co.kr.itforone.study_01_fragment.R
+import co.kr.itforone.study_01_fragment.dataClases.NonelistItem
 import co.kr.itforone.study_01_fragment.databinding.FragmentMycontBinding
 
 class Mycont :Fragment(){
     private lateinit var databind : FragmentMycontBinding
     private lateinit var mycontAdapter: MycontAdapter
+    private  var temp = mutableListOf<NonelistItem>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+        for(i in 1..10) {
+
+            temp.add(NonelistItem("test"+i, "testsrc"+i, "010-1231-123"+i))
+
+        }
+
+        Log.d("datas_size", temp.toString())
 
         databind = DataBindingUtil.inflate(inflater, R.layout.fragment_mycont, container,false)
-        mycontAdapter = MycontAdapter()
-
-
-
-
+        mycontAdapter = MycontAdapter(temp)
+        databind.myconList.layoutManager = LinearLayoutManager(activity,RecyclerView.VERTICAL, false)
+        databind.myconList.adapter = mycontAdapter
+        Log.d("datas_size", mycontAdapter.datas.size.toString())
 
         return databind.root
     }
